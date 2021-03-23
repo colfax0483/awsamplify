1c5ddc45a0401d641bfa3e2d91d344981720295f
dhpark <rharnr777@gmail.com>
2021-03-23 14:43:43 +0900

    edit js cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_Jv2yQ5s8T', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '67oe43646g13p91p7c5l6e5od1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
